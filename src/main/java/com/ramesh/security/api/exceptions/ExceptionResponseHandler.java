f74ecd8ecbe740c70a2a408c6afde11cd9a80844
Ramesh Kumar Guntamukkala <rameshkumar.gu@gmail.com>
2024-12-31 15:51:43 +0530

    Code improvements
    - Add authorize roles for /home methods
    - Hide password in response
    - Add generic exception handler
    - Add logger to print in console

--- src/main/java/com/ramesh/security/api/exceptions/ExceptionResponseHandler.java
+++ src/main/java/com/ramesh/security/api/exceptions/ExceptionResponseHandler.java
@@ -1,5 +1,7 @@
 package com.ramesh.security.api.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,13 +14,27 @@ import java.nio.file.AccessDeniedException;
 @RestControllerAdvice
 public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
 
+    Logger logger = LoggerFactory.getLogger(ExceptionResponseHandler.class);
+
     @ExceptionHandler(value = {UserNotFoundException.class})
    public ResponseEntity<Object> handleUserNotFoundException(RuntimeException ex) {
+        logIt(ex);
         return new ResponseEntity<Object>("User not found", HttpStatus.NOT_FOUND);
     }
 
    @ExceptionHandler(value = AccessDeniedException.class)
    public ResponseEntity<Object> handleAccessDeniedException(RuntimeException ex) {
+       logIt(ex);
         return new ResponseEntity<Object>("Access Denied", HttpStatus.UNAUTHORIZED);
+   }
+
+   @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<Object> handleGenericExceptions(RuntimeException ex) {
+       logIt(ex);
+        return new ResponseEntity<Object>("Unable to process the action", HttpStatus.INTERNAL_SERVER_ERROR);
+   }
+
+   private void logIt(RuntimeException ex) {
+        logger.error(ex.getMessage(), ex);
    }
 }
